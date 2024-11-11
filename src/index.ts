dcdc25a53e732b04fd07718f26e23182fc522f96
= <=>
2024-11-11 11:21:22 +0500

    Prisma setyp

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 9004;
 
@@ -16,11 +17,29 @@ const gqlServer = new ApolloServer({
             hello: String
             say(name: String): String
         }
+
+        type Mutation {
+            createUser(firstName: String!, lastName:String!, email:String!, password:String!): Boolean
+        }
     `,
     resolvers:{
         Query: {
             hello: () => 'Hello World',
             say: (_, {name}:{name:string}) => `Hey ${name} how are you`
+        },
+        Mutation:{
+            createUser: async(_,{firstName, lastName, email, password}: {firstName:string; lastName:string; email:string; password:string}) => {
+                await prismaClient.user.create({
+                    data:{
+                        firstName,
+                        lastName,
+                        email,
+                        password,
+                        salt:'random_salt'
+                    }
+                })
+                return true
+            }
         }
     }
 });
